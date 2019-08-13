13400768dce3ba667a81968897a77a049678d544
Rich Durley <richard.durley@uk.tesco.com>
2019-08-13 09:24:19 +0100

    added hints for restassured

---
+++ src/test/java/org/acme/retail/bank/account/AccountControllerTest.java
@@ -0,0 +1,32 @@
+package org.acme.retail.bank.account;
+
+import com.jayway.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest(classes = AccountApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@RunWith(SpringRunner.class)
+@ActiveProfiles("accountAPITestProfile")
+public class AccountControllerTest {
+
+    @Before
+    public void setup() {
+        RestAssured.port=8080;
+        RestAssured.baseURI="http://localhost";
+    }
+
+    public void exampleTest() {
+        //given().when().contentType("application/json").header("Authorization-X", "customer-auth-token=" + customerAuthToken).body(accountCreationRequest).post("/account/register").then()
+        //        .statusCode(SC_OK)
+        //        .body("accountId", is(accountIdToReturn.getAccountId()));
+    }
+
+
+}
